12e1e8124d7aaed9ddabb4de5bc22a04c93661dc
Kklear <kingklear@gmail.com>
2020-02-29 09:13:06 +0100

    Loading kinda done, but maybe confusing.

--- main/ui.lua
+++ main/ui.lua
@@ -7,11 +7,34 @@ function UI.load_template(template)
 		end
 		return
 	end
-	local type
+	local state, type = true
 	if pcall(gui.get_node, template.."/checkbox") then
 		type = hash("checkbox")
+		state = gui.get_flipbook(gui.get_node(template.."/checkbox")) == hash("check_true")
+	elseif pcall(gui.get_node, template.."/button_box") then
+		type = hash("button")
 	end
-	table.insert(UI.active, {template = template, type = type, state = true})
+	table.insert(UI.active, {template = template, type = type, state = state})
+end
+
+function UI.unload_template(template)
+	if template then
+		if type(template) then
+			for key, val in ipairs(template) do
+				UI.unload_template(val)
+			end
+			return
+		else
+			for key, val in ipairs(UI.active) do
+				if val.template == template then
+					table.remove(UI.active, key)
+					return
+				end
+			end
+		end
+	else
+		UI.active = {}
+	end
 end
 
 return UI
