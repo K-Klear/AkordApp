cfe034f5371449f9a8b49c959d1a38f559ffc8a8
Kklear <kingklear@gmail.com>
2020-03-07 01:35:23 +0100

    Novej koncept loadingu.

---
+++ main/sound/sound.go
@@ -0,0 +1,30 @@
+components {
+  id: "ui_close"
+  component: "/assets/sound/ui/ui_close.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "ui_open"
+  component: "/assets/sound/ui/ui_open.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
